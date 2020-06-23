e535f4d5aa727095814908aad68d062d88d106af
Jamar Torres <Jetorres88@gmail.com>
2020-06-22 22:51:50 -0400

    updated donate section and added all the organizations to donate; downloaded react-animated-slider for the info section

--- project-birthday/src/Components/Donate/Donate.js
+++ project-birthday/src/Components/Donate/Donate.js
@@ -1,26 +1,55 @@
 import React, {useState} from 'react';
 import "./Donate.scss"
-import blue from "../../images/blue_birthday_background.jpg"
+import change from "../../images/change.png"
+// import fortune from "../../images/fortunesociety.png"
+import fortune from "../../images/fortune.jpeg"
+import nysylc from "../../images/nysylc3.jpeg"
+import okra from "../../images/okra2.png"
 
 const Donate = () => {
+const cause1 = {
+        backgroundImage: 'url(' + change + ')',
+        borderRadius: '10% 0 0 0 ',
+        // marginTop: '37.5px'
+}
+
+const cause2 = {
+    backgroundImage: 'url(' + nysylc + ')',
+    borderRadius: '0 10% 0 0 ',
+    // marginTop: '37.5px'
+}
+
+const cause3 = {
+    backgroundImage: 'url(' + fortune + ')',
+    borderRadius: '0 0 0 10% '
+}
+
+const cause4 = {
+    backgroundImage: 'url(' + okra + ')',
+    borderRadius: '0 0 10% 0 ',
+    // paddingRight: '30px',
+    // paddingBottom: '199px',
+    color: 'black'
+}
     return (
     <div className="donateContainer">
         <div className="donationImageSection">
             <ul className="imageNav">
-                <li className="tabNav"><a><span className="imgHolder" style={{backgroundImage: 'url('+blue+') '}}><section className="title">Black Lives</section></span></a></li>
-                <li className="tabNav"><a><section className="title">Latinx Power</section><span className="imgHolder" src=""></span></a></li>
-                <li className="tabNav"><a><section className="title">Black Trans</section><span className="imgHolder" src=""></span></a></li>
-                <li className="tabNav"><a><section className="title">Mental Health</section><span className="imgHolder" src=""></span></a></li>
+                <li className="tabNav"><a href="https://secure.actblue.com/contribute/page/support-us"><section className="title2">Color of Change</section><span className="imgHolder" style={cause1}></span></a></li>
+                <li className="tabNav"><a href="https://www.nysylc.org/donate"><section className="title2"> NYSYLC </section><span className="imgHolder" style={cause2}></span></a></li>
+                <li className="tabNav"><a href="https://fortunesociety.org/ways-to-give/"><span className="imgHolder" style={cause3}></span><section className="title2">The Fortune Society</section></a></li>
+                <li className="tabNav"><a href="https://www.theokraproject.com/"><span className="imgHolder" style={cause4}></span><section className="title2">The Okra Project</section></a></li>
             </ul>
         </div>
         <div className="donationInfoSection">
-            <h2>Donate to a cause</h2>
+            <h1>Donate to a cause</h1>
         
-            <h3>Please donate to one of four causes</h3>
-            that are near and dear to my heart. 
+            <div className="info">Please donate to one of four causes
+                 that are near and dear to my heart. 
                 These donations go to a local based organization that is in complete support of the movements, ranging from ....
                 Org 1 supports the Black Lives Matter movement, Org 2 supports the Latinx in that, Org 3 supports Mental Health Endeavour, 
                 and Org 4 supports formely incarcerated men who are in need of mental health services to get their feet off the ground. 
+            </div>
         </div>
     </div>
     )
--- project-birthday/src/Components/Info/Info.js
+++ project-birthday/src/Components/Info/Info.js
@@ -1,21 +1,37 @@
 import React, {useState} from 'react';
+import Slider from 'react-animated-slider';
+import horizontalCss from 'react-animated-slider/build/horizontal.css';
+import "./Info.scss";
 
 const Info = () => {
+
+    const content = [
+        { title: 'First item', description: 'Lorem ipsum', button: 'hello'},
+        { title: 'Second item', description: 'Lorem ipsum', button: 'hey'}
+    ]
     return (
-    <div>
+    <div className="infoContainer">
         <h2>Info</h2>
-        <h3>With shelter in place still being in effect somewhat, it's important to move around! Join me for fitness classes!</h3>
-
-        <div>Workouts!
-
-            <div>Body by Kadiddy!
-
-            </div>
-
-            <div>Zumba by ____!
-            </div>
-
+        <div>
+        Charmander pokeball team rocket char pikachu jiggly puff pokedex i choose you sycther gameboy colour battle onyx i choose you trainer gameboy colour pokedex thunderbolt i choose you sycther gameboy colour mew jiggly puff pikachu onyx pokedex pikachu trainer abra ash mew gameboy colour a wild bulbasaur mew-two meaowth charmander pikachu growlith team rocket mew-two. Bulbasaur pokedex ash gym battle jiggly puff blasting of again onyx bulbasaur professor rowan a wild growlith trainer team rocket team rocket abra squirtle jiggly puff growlith ash pokedex a wild pokeball mew-two thunderbolt thunderbolt i choose you growlith jiggly puff abra pokedex ace trainer pokeball pikachu blasting of again thunderbolt charmander battle gym battle gym battle squirtle.
         </div>
+  
+
+<Slider classNames={horizontalCss}>
+{content.map((item, index) => (
+	<div
+		key={index}
+		style={{ background: `url('${item.image}') no-repeat center center` }}
+	>
+		<div className="center">
+			<h1>{item.title}</h1>
+			<p>{item.description}</p>
+			<button>{item.button}</button>
+		</div>
+	</div>
+))}
+</Slider>
+        
 
     </div>
     )
