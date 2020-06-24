7d08f97c503957a40a05813a1118fd886e18906d
Jamar Torres <Jetorres88@gmail.com>
2020-06-23 22:46:45 -0400

    added animation to the project header

--- project-birthday/src/App.js
+++ project-birthday/src/App.js
@@ -12,7 +12,9 @@ function App() {
     <div className="App">
     <NavBar />
       <div className="container">
-        <h1 className='title'>Project Birthday!</h1>
+        <div className="targetElement">
+        <div className='animateTitle'>Project Birthday!</div>
+        </div>
 
       <div className="threePanelSection">
         <Panel title={"Project Birthday"} description={"Charmander pokeball team rocket char pikachu jiggly puff pokedex i choose you sycther gameboy colour battle onyx i choose you trainer gameboy colour pokedex thunderbolt i choose you sycther gameboy colour mew jiggly puff pikachu onyx pokedex pikachu trainer abra ash mew gameboy colour a wild bulbasaur mew-two meaowth charmander pikachu growlith team rocket mew-two. Bulbasaur pokedex ash gym battle jiggly puff blasting of again onyx bulbasaur professor rowan a wild growlith trainer team rocket team rocket abra squirtle jiggly puff growlith ash pokedex a wild pokeball mew-two thunderbolt thunderbolt i choose you growlith jiggly puff abra pokedex ace trainer pokeball pikachu blasting of again thunderbolt charmander battle gym battle gym battle squirtle."} />
